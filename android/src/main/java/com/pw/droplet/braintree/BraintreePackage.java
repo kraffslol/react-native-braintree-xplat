45572cf72e70d13dd337ce4aa6a9e39ad712651b
Rickard Ekman <xm0rfin@gmail.com>
2016-06-27 14:03:01 +0200

    React-Native 0.29 Compability

--- android/src/main/java/com/pw/droplet/braintree/BraintreePackage.java
+++ android/src/main/java/com/pw/droplet/braintree/BraintreePackage.java
@@ -1,8 +1,5 @@
 package com.pw.droplet.braintree;
 
-import android.content.Intent;
-import android.content.Context;
-
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
@@ -14,17 +11,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class BraintreePackage implements ReactPackage {
-  private Context mContext;
   private Braintree mModuleInstance;
-
-  public BraintreePackage(Context activityContext) {
-    mContext = activityContext;
-  }
 
   @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
     List<NativeModule> modules = new ArrayList<>();
-    mModuleInstance = new Braintree(reactContext, mContext);
+    mModuleInstance = new Braintree(reactContext);
 
     modules.add(mModuleInstance);
     return modules;
@@ -38,9 +30,5 @@ public class BraintreePackage implements ReactPackage {
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Collections.emptyList();
-  }
-
-  public void handleActivityResult(final int requestCode, final int resultCode, final Intent data) {
-    mModuleInstance.handleActivityResult(requestCode, resultCode, data);
   }
 }
