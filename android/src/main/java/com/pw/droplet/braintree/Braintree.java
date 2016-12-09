kraffslol/react-native-braintree-xplat

commit d8757575e27e21dbfbba6f4363b738e279f911ec

Pass CVV in Android

android/src/main/java/com/pw/droplet/braintree/Braintree.java
@@ -66,14 +66,16 @@ public class Braintree extends ReactContextBaseJavaModule implements ActivityEve
   }
 
   @ReactMethod
-  public void getCardNonce(final String cardNumber, final String expirationMonth, final String expirationYear, final Callback successCallback, final Callback errorCallback) {
+  public void getCardNonce(final String cardNumber, final String expirationMonth, final String expirationYear, final String cvv, final Callback successCallback, final Callback errorCallback) {
     this.successCallback = successCallback;
     this.errorCallback = errorCallback;
 
     CardBuilder cardBuilder = new CardBuilder()
       .cardNumber(cardNumber)
       .expirationMonth(expirationMonth)
-      .expirationYear(expirationYear);
+      .expirationYear(expirationYear)
+      .cvv(cvv)
+      .validate(true);
 
     Card.tokenize(this.mBraintreeFragment, cardBuilder);
   }

index.android.js
@@ -10,9 +10,9 @@ module.exports = {
     });
   },
 
-  getCardNonce(cardNumber, expirationMonth, expirationYear) {
+  getCardNonce(cardNumber, expirationMonth, expirationYear, cvv) {
     return new Promise(function(resolve, reject) {
-      Braintree.getCardNonce(cardNumber, expirationMonth, expirationYear, (nonce) => resolve(nonce), (err) => reject(err))
+      Braintree.getCardNonce(cardNumber, expirationMonth, expirationYear, cvv, (nonce) => resolve(nonce), (err) => reject(err))
     });
   },
 

commit 5893cd96c21d68761f303a1c7f8653ae0c52be37

change the CardBuilder Validate to false
resolves: https://github.com/kraffslol/react-native-braintree-xplat/issues/20
this bug was introduced by https://github.com/kraffslol/react-native-braintree-xplat/commit/d8757575e27e21dbfbba6f4363b738e279f911ec on Aug 10.

As a result, when you use an invalid card, the function call hangs and catch is never called.

android/src/main/java/com/pw/droplet/braintree/Braintree.java
@@ -75,7 +75,7 @@ public class Braintree extends ReactContextBaseJavaModule implements ActivityEve
       .expirationMonth(expirationMonth)
       .expirationYear(expirationYear)
       .cvv(cvv)
-      .validate(true);
+      .validate(false);
 
     Card.tokenize(this.mBraintreeFragment, cardBuilder);
   }