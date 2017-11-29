acc694abdce97e940888fd52962dda5502781176
Andrej Badin <contact@andrejbadin.com>
2017-11-29 13:19:45 +0100

    Update types

--- types.js
+++ types.js
@@ -1,47 +1,43 @@
 // @flow
 
 export type CardParameters = {
-    number: string,
-    expirationMonth: string,
-    expirationYear: string,
-    cvv: string,
-    expirationDate: string,
-    cardholderName: string,
-    billingAddress: string,
-    firstName: string,
-    lastName: string,
-    company: string,
+  number: string,
+  cvv: string,
+  expirationDate: string,
+  cardholderName: string,
+  firstName: string,
+  lastName: string,
+  company: string,
+  countryName: string,
+  countryCodeAlpha2: string,
+  countryCodeAlpha3: string,
+  countryCodeNumeric: string,
+  locality: string,
+  postalCode: string,
+  region: string,
+  streetAddress: string,
+  extendedAddress: string,
+};
+
+export type IOSCardParameters = {
+  number: string,
+  cvv: string,
+  expirationDate: string,
+  cardholderName: string,
+  billingAddress: {
+    postalCode: string,
+    streetAddress: string,
+    extendedAddress: string,
+    locality: string,
+    region: string,
     countryName: string,
     countryCodeAlpha2: string,
     countryCodeAlpha3: string,
     countryCodeNumeric: string,
-    locality: string,
-    postalCode: string,
-    region: string,
-    streetAddress: string,
-    extendedAddress: string,
-  };
+    firstName: string,
+    lastName: string,
+    company: string,
+  },
+};
 
-  export type IOSCardParameters = {
-    number: string,
-    expirationMonth: string,
-    expirationYear: string,
-    cardholderName: string,
-    billingAddress: {
-      postalCode: string,
-      streetAddress: string,
-      extendedAddress: string,
-      locality: string,
-      region: string,
-      countryName: string,
-      countryCodeAlpha2: string,
-      countryCodeAlpha3: string,
-      countryCodeNumeric: string,
-      firstName: string,
-      lastName: string,
-      company: string,
-    },
-  };
-  };
-
-  export type AndroidCardParameters = CardParameters;
+export type AndroidCardParameters = CardParameters;
